7009435fa03640594721e13d865e7730a1003bd4
Gary Yang <gary.y.7811@gmail.com>
2025-06-24 23:15:14 -0400

    update++

--- bin/rag-vector-storage-service.ts
+++ bin/rag-vector-storage-service.ts
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { StackProps } from 'aws-cdk-lib';
-import { RagVectorStorageServiceStack } from '../lib/rag-vector-storage-service-stack';
-import { RagContracts, RagVectorStorageEnver } from '@odmd-rag/contracts-lib-rag';
+import {RagVectorStorageServiceStack} from '../lib/rag-vector-storage-service-stack';
+import {RagContracts, RagVectorStorageEnver} from '@odmd-rag/contracts-lib-rag';
 
 const app = new cdk.App({autoSynth: false});
 
@@ -14,13 +13,11 @@ async function main() {
         throw new Error("buildRegion>" + buildRegion + "; buildAccount>" + buildAccount);
     }
 
-
     new RagContracts(app);
 
     const targetEnver = RagContracts.inst.getTargetEnver() as RagVectorStorageEnver;
 
-    // Create the main vector storage stack using OnDemandEnv pattern
-    const mainStack = new RagVectorStorageServiceStack(app, targetEnver, {
+    new RagVectorStorageServiceStack(app, targetEnver, {
         env: {
             account: buildAccount,
             region: buildRegion
